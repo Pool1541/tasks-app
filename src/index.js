d65997fcc9c0d17921380b16c71d9a9e138cbc19
Pool <pool_1541@hotmail.com>
2023-03-11 19:43:39 -0500

    change browserRoute for hashRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Dashboard from './routes/dashboard';
-import RegisterUsername from './routes/registerUsername';
-import { ThemeContextProvider } from './context/themeContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Dashboard from "./routes/dashboard";
+import RegisterUsername from "./routes/registerUsername";
+import { ThemeContextProvider } from "./context/themeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='Register-username' element={<RegisterUsername />} />
+        <Route path="/" element={<App />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="Register-username" element={<RegisterUsername />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </ThemeContextProvider>
 );
